5a7c8266309da4ad5021ad7ee72ff0026d537931
Shepard Emerson <shepard.emerson@dresden.us>
2011-09-11 22:10:22 -0400

    Completed Classes
    Monologue and Database classes completed. Search functionality added.
    May be vulnerable to sql injection through search… not sure how to fix
    that.

--- MonologueClasses.php
+++ MonologueClasses.php
@@ -2,53 +2,71 @@
 
 /* Database class */
 class Database {
-	private $database = "monologuedb";
-	private $tableName = "monologue";
-	private $monologues = array();
+	// Database Variables. You need to set these.
+	// I tried to make these static but php doesn't like it.
+	private $database = "DATABASENAME";
+	private $tableName = "TABLENAME";
+	private $username = "USERNAME";
+	private $password = "PASSWORD";
+	private $server = "SERVER";
+	
+	// Connection variable
+	private $con;
 
 	// Constructor.
-	public function __construct($server, $username, $password) {
-		// Initialize mysql
-		$con = mysql_connect($server, $username, $password);
-		if (!$con) die ('Could not connect: ' . mysql_error());
-		mysql_select_db($this->database,$con);
-		
-		// Get contents
-		$query = "SELECT * FROM " . $this->tableName;
-		$result = mysql_query($query,$con);
-		
-		// Organize into objects
-		while($row = mysql_fetch_array($result)) {
-			$this->monologues[] = new Monologue($row);
-		}
+	public function __construct() {
+		// Initialize MySQL
+		$this->con = mysql_connect($this->server, $this->username, $this->password);
+		if (!$this->con) die ('Could not connect: ' . mysql_error());
+		mysql_select_db($this->database,$this->con);
 	}
 	
+	// Search Function
 	public function rowsForSearch() {
-		$goodMonologues = $this->monologues;
+		// Start Query
+		$query = "SELECT * FROM " . $this->tableName . " WHERE ";
+		$monologues = array();
+		
+		// Go through arguments
 		if(func_num_args() > 0) {
 			for($i = 0; $i < func_num_args(); $i++) {
 				$argument = func_get_arg($i);
 				
-				//debug
-				echo $i."<br>";
-				echo $argument;
-				
+				// If argument is a string
 				if(is_string($argument)) {
-					$goodMonologues = $this->monologuesWithString($goodMonologues, $argument);
+					$query .= "(Title LIKE '%".$argument."%' OR 
+							   Author LIKE '%".$argument."%' OR 
+							   Monologue LIKE '%".$argument."%') ";
 				}
+				
+				// If argument is an array
+				else if(is_array($argument)) {
+					$number = count($argument);
+					$counter = 0;
+					foreach ($argument as $k=>$v) {
+						$query .= "".$k."='".$v."' ";
+						$counter++;
+						if($counter!=$number) $query .= "AND ";
+					}
+				}
+				
+				// Add an "AND" on the end if necessary
+				if($i != func_num_args()-1) $query .= "AND ";
+				
 			}
-			return $goodMonologues;
+			
+			// DEBUG
+			// echo $query;
+			
+			// Query database, store results in array
+			if($result = mysql_query($query, $this->con)) {
+				while($row = mysql_fetch_array($result)) {
+          	 		$this->monologues[] = new Monologue($row);
+        		}
+        		return $this->monologues;
+        	}
 		}
-		else {
-			echo "Nothing Searched.";
-		}
-	}
-	
-	private function monologuesWithString($monologues, $string) {
-		$goodMonologues = array();
-		for($i = 0; $i<count($monologues); $i++) {
-			if($monologues[i]->inMonologue($string)) $goodMonologues[] = $monologues[i];
-		}
+		else echo "Nothing Searched.";
 	}
 }
 
@@ -73,18 +91,23 @@ class Monologue {
  		
  		return $return;
 	}
-	
-	public function inMonologue($string) {
-		if(in_array($string, $this->attributes)) {
-			echo $this->attributes['ID']."->YES";
-			return TRUE;
-		}
-		
-	} 
+
 }
 
-$myDatabase = new Database("127.0.0.1","root","sniggle");
-$mySearch = $myDatabase->rowsForSearch("testicle");
-echo count($mySearch);
+// Make a new database, search it
+$myDatabase = new Database();
+$mySearch = $myDatabase->rowsForSearch("the", array("Gender"=>"G01", "WrittenInPeriod"=>"W04"), "it", "kristen dabrowski");
+
+
+// Print results, for testing purposes
+echo "<br><br><br>";
+echo "<b>Results:</b><br>";
+echo count($mySearch)." records found.";
+if(count($mySearch)) {
+	echo "They are:<br>";
+	foreach($mySearch as $result ) {
+		echo $result->getElement("Title")." by ".$result->getElement("Author")."<br>";
+	}
+}
 
 ?>
